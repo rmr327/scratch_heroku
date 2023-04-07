5f759a44f8a5f4a768640b49a5d581b08d8beda8
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-04-07 18:44:41 -0400

    Add files via upload

--- NbaStatsDash.py
+++ NbaStatsDash.py
@@ -159,6 +159,9 @@ if __name__ == '__main__':
         team__1 = game[0]
         team__2 = game[1]
         if st.checkbox(f"{team__1} VS {team__2}"):
+            # Define the main tabs
+            tabs = ["score_diff_probability", "Head to Head", "scaled_points_stats", "Raw Stats"]
+            main_tab = st.radio("Select a tab", tabs)
 
             team_1__id = get_team_id(teams__df, team__1)
             team_2__id = get_team_id(teams__df, team__2)
@@ -255,43 +258,46 @@ if __name__ == '__main__':
             score_diff_probability_df = pd.DataFrame(data['Score Difference Probability'], index=['probability']).T
 
             st.subheader(f"{team__1} VS {team__2}")
-            st.text(f"Head to Head")
-            col1, col2 = st.columns(2)
-            with col1:
-                try:
-                    st.dataframe(head_to_head_df)
-                except ValueError:
-                    st.text("Head to Head data fame not available.")
-            with col2:
-                try:
-                    st.dataframe(head_to_head_stats_df)
-                except ValueError:
-                    st.text('Head to Head stats data frame not available.')
-
             all_scores = list(range(50, 170))
-            m1 = head_to_head_stats_df[team__1]['mean']
-            s1 = head_to_head_stats_df[team__1]['std']
-            m2 = head_to_head_stats_df[team__2]['mean']
-            s2 = head_to_head_stats_df[team__2]['std']
+            if main_tab == 'Head to Head':
+                st.text(f"Head to Head")
+                col1, col2 = st.columns(2)
+                with col1:
+                    try:
+                        st.dataframe(head_to_head_df)
+                    except ValueError:
+                        st.text("Head to Head data fame not available.")
+                with col2:
+                    try:
+                        st.dataframe(head_to_head_stats_df)
+                    except ValueError:
+                        st.text('Head to Head stats data frame not available.')
 
-            h_2_h_team_1_graph = h_2h_pdf_plot_helper(all_scores, m1, s1, 'black', team__1, 'PDF')
-            h_2_h_team_2_graph_pdf = h_2h_pdf_plot_helper(all_scores, m2, s2, 'red', team__2, 'PDF', h_2_h_team_1_graph)
-            h_2_h_team_2_graph_pdf.legend.location = 'top_left'
+                m1 = head_to_head_stats_df[team__1]['mean']
+                s1 = head_to_head_stats_df[team__1]['std']
+                m2 = head_to_head_stats_df[team__2]['mean']
+                s2 = head_to_head_stats_df[team__2]['std']
 
-            crosshair = CrosshairTool(line_alpha=0.4, line_color='#1f77b4')
-            h_2_h_team_2_graph_pdf.add_tools(crosshair)
+                h_2_h_team_1_graph = h_2h_pdf_plot_helper(all_scores, m1, s1, 'black', team__1, 'PDF')
+                h_2_h_team_2_graph_pdf = h_2h_pdf_plot_helper(all_scores, m2, s2, 'red', team__2, 'PDF',
+                                                              h_2_h_team_1_graph)
+                h_2_h_team_2_graph_pdf.legend.location = 'top_left'
 
-            h_2_h_team_1_graph = h_2h_cdf_plot_helper(all_scores, m1, s1, 'black', team__1, 'CDF')
-            h_2_h_team_2_graph_cdf = h_2h_cdf_plot_helper(all_scores, m2, s2, 'red', team__2, 'CDF', h_2_h_team_1_graph)
-            h_2_h_team_2_graph_cdf.legend.location = 'top_left'
-            h_2_h_team_2_graph_cdf.add_tools(crosshair)
+                crosshair = CrosshairTool(line_alpha=0.4, line_color='#1f77b4')
+                h_2_h_team_2_graph_pdf.add_tools(crosshair)
 
-            col1, col2 = st.columns(2)
-            with col1:
-                try:
-                    st.bokeh_chart(column(h_2_h_team_2_graph_pdf, h_2_h_team_2_graph_cdf))
-                except ValueError:
-                    st.text('Head to Head distribution plots not available.')
+                h_2_h_team_1_graph = h_2h_cdf_plot_helper(all_scores, m1, s1, 'black', team__1, 'CDF')
+                h_2_h_team_2_graph_cdf = h_2h_cdf_plot_helper(all_scores, m2, s2, 'red', team__2, 'CDF',
+                                                              h_2_h_team_1_graph)
+                h_2_h_team_2_graph_cdf.legend.location = 'top_left'
+                h_2_h_team_2_graph_cdf.add_tools(crosshair)
+
+                col1, col2 = st.columns(2)
+                with col1:
+                    try:
+                        st.bokeh_chart(column(h_2_h_team_2_graph_pdf, h_2_h_team_2_graph_cdf))
+                    except ValueError:
+                        st.text('Head to Head distribution plots not available.')
 
             vdf['GAME_DATE'] = pd.to_datetime(vdf['GAME_DATE'])
             vdf['dt'] = vdf['GAME_DATE'].dt.strftime('%Y-%m-%d %H:%M:%S')
@@ -315,123 +321,126 @@ if __name__ == '__main__':
                                                      (f"{team__2}", "@PTS_2")], renderers=[xx, yy])
             pp.add_tools(hov_)
 
-            st.text(f"scaled_points_stats")
-            try:
-                st.dataframe(scaled_points_stats_df)
-            except ValueError:
-                st.text('Scaled Points stats data frame not available')
-
-            t1['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
-            t2['GAME_DATE'] = pd.to_datetime(t2['GAME_DATE'])
-
-            pan = PanTool(dimensions='width')
-            p_r = figure(title=f"Season Points Scaled", x_axis_label='Date', y_axis_label='points',
-                         x_axis_type='datetime', plot_width=1000, plot_height=600,
-                         tools=[pan, 'xwheel_zoom'], active_scroll='xwheel_zoom')
-            p_r = raw_plots_helper(t1, team__1, p_r, 'black')
-            p_r = raw_plots_helper(t2, team__2, p_r, 'red')
-
-            try:
-                st.bokeh_chart(p_r)
-            except ValueError:
-                st.text('raw scaled plots not available')
-
-            with col2:
+            if main_tab == 'scaled_points_stats':
+                st.text(f"scaled_points_stats")
                 try:
-                    st.bokeh_chart(column(pp0, pp))
+                    st.dataframe(scaled_points_stats_df)
                 except ValueError:
-                    st.text('Head to Head Diff anf Raw plots not available')
+                    st.text('Scaled Points stats data frame not available')
 
-            col1, col2 = st.columns(2)
+                t1['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
+                t2['GAME_DATE'] = pd.to_datetime(t2['GAME_DATE'])
 
-            with col1:
-                m1 = scaled_points_stats_df[f'{team__1}_season']['mean']
-                s1 = scaled_points_stats_df[f'{team__1}_season']['stdev']
-                m2 = scaled_points_stats_df[f'{team__2}_season']['mean']
-                s2 = scaled_points_stats_df[f'{team__2}_season']['stdev']
-                scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'All Season')
+                pan = PanTool(dimensions='width')
+                p_r = figure(title=f"Season Points Scaled", x_axis_label='Date', y_axis_label='points',
+                             x_axis_type='datetime', plot_width=1000, plot_height=600,
+                             tools=[pan, 'xwheel_zoom'], active_scroll='xwheel_zoom')
+                p_r = raw_plots_helper(t1, team__1, p_r, 'black')
+                p_r = raw_plots_helper(t2, team__2, p_r, 'red')
 
-                m1 = scaled_points_stats_df[f'{team__1}_last_10']['mean']
-                s1 = scaled_points_stats_df[f'{team__1}_last_10']['stdev']
-                m2 = scaled_points_stats_df[f'{team__2}_last_10']['mean']
-                s2 = scaled_points_stats_df[f'{team__2}_last_10']['stdev']
-                scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 10 Games')
-
-            with col2:
-                m1 = scaled_points_stats_df[f'{team__1}_last_15']['mean']
-                s1 = scaled_points_stats_df[f'{team__1}_last_15']['stdev']
-                m2 = scaled_points_stats_df[f'{team__2}_last_15']['mean']
-                s2 = scaled_points_stats_df[f'{team__2}_last_15']['stdev']
-                scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 15 Games')
-
-                m1 = scaled_points_stats_df[f'{team__1}_last_5']['mean']
-                s1 = scaled_points_stats_df[f'{team__1}_last_5']['stdev']
-                m2 = scaled_points_stats_df[f'{team__2}_last_5']['mean']
-                s2 = scaled_points_stats_df[f'{team__2}_last_5']['stdev']
-                scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 5 Games')
-
-            st.text(f"score_diff_probability")
-            col1, col2 = st.columns(2)
-
-            with col2:
                 try:
-                    st.dataframe(score_diff_probability_df.style.background_gradient(cmap="YlGnBu"))
+                    st.bokeh_chart(p_r)
                 except ValueError:
-                    st.text('Score Diff Probability data frame not available')
+                    st.text('raw scaled plots not available')
 
-            all_diffs = list(range(0, 50))
-            m1 = score_diff_probability_df.mean()
-            s1 = score_diff_probability_df.std()
-            p_1 = h_2h_pdf_plot_helper(all_diffs, m1, s1, 'black', 'PDF', 'PDF')
-            p_2 = h_2h_cdf_plot_helper(all_diffs, m1, s1, 'black', 'CDF', 'CDF')
+            if main_tab == 'Head to Head':
+                with col2:
+                    try:
+                        st.bokeh_chart(column(pp0, pp))
+                    except ValueError:
+                        st.text('Head to Head Diff anf Raw plots not available')
+                col1, col2 = st.columns(2)
 
-            with col1:
+                with col1:
+                    m1 = scaled_points_stats_df[f'{team__1}_season']['mean']
+                    s1 = scaled_points_stats_df[f'{team__1}_season']['stdev']
+                    m2 = scaled_points_stats_df[f'{team__2}_season']['mean']
+                    s2 = scaled_points_stats_df[f'{team__2}_season']['stdev']
+                    scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'All Season')
+
+                    m1 = scaled_points_stats_df[f'{team__1}_last_10']['mean']
+                    s1 = scaled_points_stats_df[f'{team__1}_last_10']['stdev']
+                    m2 = scaled_points_stats_df[f'{team__2}_last_10']['mean']
+                    s2 = scaled_points_stats_df[f'{team__2}_last_10']['stdev']
+                    scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 10 Games')
+
+                with col2:
+                    m1 = scaled_points_stats_df[f'{team__1}_last_15']['mean']
+                    s1 = scaled_points_stats_df[f'{team__1}_last_15']['stdev']
+                    m2 = scaled_points_stats_df[f'{team__2}_last_15']['mean']
+                    s2 = scaled_points_stats_df[f'{team__2}_last_15']['stdev']
+                    scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 15 Games')
+
+                    m1 = scaled_points_stats_df[f'{team__1}_last_5']['mean']
+                    s1 = scaled_points_stats_df[f'{team__1}_last_5']['stdev']
+                    m2 = scaled_points_stats_df[f'{team__2}_last_5']['mean']
+                    s2 = scaled_points_stats_df[f'{team__2}_last_5']['stdev']
+                    scaled_plot_helper(m1, s1, m2, s2, all_scores, team__1, team__2, 'Last 5 Games')
+
+            if main_tab == 'score_diff_probability':
+                st.text(f"score_diff_probability")
+                col1, col2 = st.columns(2)
+
+                with col2:
+                    try:
+                        st.dataframe(score_diff_probability_df.style.background_gradient(cmap="YlGnBu"))
+                    except ValueError:
+                        st.text('Score Diff Probability data frame not available')
+
+                all_diffs = list(range(0, 50))
+                m1 = score_diff_probability_df.mean()
+                s1 = score_diff_probability_df.std()
+                p_1 = h_2h_pdf_plot_helper(all_diffs, m1, s1, 'black', 'PDF', 'PDF')
+                p_2 = h_2h_cdf_plot_helper(all_diffs, m1, s1, 'black', 'CDF', 'CDF')
+
+                with col1:
+                    try:
+                        st.bokeh_chart(column(p_1, p_2))
+                    except ValueError:
+                        st.text('Score Diff Probability data frame not available')
+
+            if main_tab == 'Raw Stats':
+                st.text('Raw Stats')
+                t1 = team__log_dict_[team__1]
+                t1['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
+                t1 = t1.dropna(subset=['GAME_DATE'])
+                t2 = team__log_dict_[team__2]
+                t2['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
+                t2 = t2.dropna(subset=['GAME_DATE'])
+
+                pan = PanTool(dimensions='width')
+                p_r = figure(title=f"Season Points", x_axis_label='Date', y_axis_label='points', x_axis_type='datetime',
+                             plot_width=1000, plot_height=600, tools=[pan, 'xwheel_zoom'], active_scroll='xwheel_zoom')
+                p_r = raw_plots_helper(t1, team__1, p_r, 'black')
+                p_r = raw_plots_helper(t2, team__2, p_r, 'red')
+
                 try:
-                    st.bokeh_chart(column(p_1, p_2))
+                    if st.checkbox(f"See detail game histories"):
+                        st.caption(team__1)
+                        t1s = t1.style.applymap(color_by_score, subset=["WL"])
+                        t1s = t1s.background_gradient(cmap="YlGnBu")
+                        st.dataframe(t1s)
+                        st.caption(team__2)
+                        t2s = t2.style.applymap(color_by_score, subset=["WL"])
+                        t2s = t2s.background_gradient(cmap="YlGnBu")
+
+                        st.dataframe(t2s)
+                    st.bokeh_chart(p_r)
+
+                    # Create a multiselect widget to allow the user to select columns
+                    column2 = st.selectbox("Select column to plot", t2.columns)
+
+                    pan_ = PanTool(dimensions='width')
+                    f = figure(title=f"{column2} Time series", x_axis_label='DT', y_axis_label=column2,
+                               x_axis_type='datetime', plot_width=1000, plot_height=600, tools=[pan_, 'xwheel_zoom'],
+                               active_scroll='xwheel_zoom')
+                    f = raw_plots_helper(t1, f"{team__1} {column2}", f, 'black', 1, column2)
+                    f = raw_plots_helper(t2, f"{team__2} {column2}", f, 'red', 1, column2)
+
+                    st.bokeh_chart(f, use_container_width=True)
+
                 except ValueError:
-                    st.text('Score Diff Probability data frame not available')
-
-            st.text('Raw Stats')
-            t1 = team__log_dict_[team__1]
-            t1['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
-            t1 = t1.dropna(subset=['GAME_DATE'])
-            t2 = team__log_dict_[team__2]
-            t2['GAME_DATE'] = pd.to_datetime(t1['GAME_DATE'])
-            t2 = t2.dropna(subset=['GAME_DATE'])
-
-            pan = PanTool(dimensions='width')
-            p_r = figure(title=f"Season Points", x_axis_label='Date', y_axis_label='points', x_axis_type='datetime',
-                         plot_width=1000, plot_height=600, tools=[pan, 'xwheel_zoom'], active_scroll='xwheel_zoom')
-            p_r = raw_plots_helper(t1, team__1, p_r, 'black')
-            p_r = raw_plots_helper(t2, team__2, p_r, 'red')
-
-            try:
-                if st.checkbox(f"See detail game histories"):
-                    st.caption(team__1)
-                    t1s = t1.style.applymap(color_by_score, subset=["WL"])
-                    t1s = t1s.background_gradient(cmap="YlGnBu")
-                    st.dataframe(t1s)
-                    st.caption(team__2)
-                    t2s = t2.style.applymap(color_by_score, subset=["WL"])
-                    t2s = t2s.background_gradient(cmap="YlGnBu")
-
-                    st.dataframe(t2s)
-                st.bokeh_chart(p_r)
-
-                # Create a multiselect widget to allow the user to select columns
-                column2 = st.selectbox("Select column to plot", t2.columns)
-
-                pan_ = PanTool(dimensions='width')
-                f = figure(title=f"{column2} Time series", x_axis_label='DT', y_axis_label=column2,
-                           x_axis_type='datetime', plot_width=1000, plot_height=600, tools=[pan_, 'xwheel_zoom'],
-                           active_scroll='xwheel_zoom')
-                f = raw_plots_helper(t1, f"{team__1} {column2}", f, 'black', 1, column2)
-                f = raw_plots_helper(t2, f"{team__2} {column2}", f, 'red', 1, column2)
-
-                st.bokeh_chart(f, use_container_width=True)
-
-            except ValueError:
-                st.text('Raw data not available')
+                    st.text('Raw data not available')
 
     try:
         # replace with images/illusion.jpg when deploying
